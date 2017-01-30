1b7137cf9d2f408bb0b006508207341afab40109
Jonester <chris.jones78@gmail.com>
2017-01-30 11:23:34 -0500

    Initial Commit

---
+++ iPhoneImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhoneImages
+//
+//  Created by Chris Jones on 2017-01-30.
+//  Copyright © 2017 Jonescr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPhoneImages/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iPhoneImages
+//
+//  Created by Chris Jones on 2017-01-30.
+//  Copyright © 2017 Jonescr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
